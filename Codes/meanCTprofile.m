3d5046ee025cc8078539d897f13e2cb0d472bd0d
Yan Yu Ting <49742554+yuting-yan@users.noreply.github.com>
2020-12-21 20:15:00 +0800

    Update meanCTprofile.m

--- Codes/meanCTprofile.m
+++ Codes/meanCTprofile.m
@@ -23,3 +23,15 @@ ll = ct_mean - ct_sd; % calculate lower limit
 Ulim = [Ulim; ul]; % store the results
 Llim = [Llim; ll]; % store the results
 end
+
+%% identify the threshold for detecting changes in data
+change_thresh = Threshold;
+            
+mean_cal = all_mean (~isnan(all_mean)); % removing Nan values
+            
+% identifying points of changes
+pts = findchangepts (mean_cal, 'Statistic', 'rms',...
+                'MinThreshold',change_thresh);
+            
+% convert the points to depth
+pts_depth = pts*info.SliceThickness/10;
