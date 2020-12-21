6978930dd7063528f3dcd13098ca33a079d3185a
Yan Yu Ting <49742554+yuting-yan@users.noreply.github.com>
2020-12-21 19:14:47 +0800

    Create Import.m

---
+++ Code/Import.m
@@ -0,0 +1,43 @@
+
+
+
+%% Set the directory
+path = uigetdir;
+
+filefolder = fullfile (path);
+cd(path)
+
+d = dir;
+names = {d.name};
+names(:,1:3) = [];
+
+info = dicominfo (fullfile (filefolder, names {5}));
+numimages = length (5:length(names));
+
+Original = zeros (info.Rows, info.Columns, numimages, class(info.BitDepth));
+%% rearranging the 3D matrix according to the instance number
+
+h = waitbar(0, 'Loading CT data');
+order = [];
+
+% for all images in the folder
+for i = 1: length (names)
+    
+% read the instance number
+fname = fullfile(filefolder, names{i});
+info = dicominfo(fname);
+instance_no = info.InstanceNumber;
+
+% insert the image in the proper location of the matrix
+Original (:,:,instance_no) = uint16 (dicomread (fname));
+
+order (instance_no) = i;
+
+waitbar (i/length(names),h);
+end
+
+close (h)
+
+%% converting from gray values to HU
+Original = int16 (Original); % change from unsigned to signed integer to accomodate negative values
+Original = (Original*info.RescaleSlope) + info.RescaleIntercept; % changing to HU
