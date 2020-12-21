5107004a9ece05e9320811d026e1f8d47b2afd6a
Yan Yu Ting <49742554+yuting-yan@users.noreply.github.com>
2020-12-21 20:18:43 +0800

    Update CropDICOM.m

--- Codes/CropDICOM.m
+++ Codes/CropDICOM.m
@@ -1,3 +1,26 @@
+
+%  This code crops the DICOM volume according to user-defined mask
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%     This file is part of Core-CT. Copyright (C) 2020  Yu Ting Yan
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%      
+%     Please report any bug, error or suggestion to yuting004@e.ntu.edu.sg
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %% For a circle mask
 % size of the circle that I want
 Rthres = 170; 
