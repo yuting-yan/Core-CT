a89343c6875cce04a510799010c30d9f72efff73
Yan Yu Ting <49742554+yuting-yan@users.noreply.github.com>
2020-12-21 19:17:20 +0800

    Update Import.m

--- Codes/Import.m
+++ Codes/Import.m
@@ -11,13 +11,15 @@ d = dir;
 names = {d.name};
 names(:,1:3) = [];
 
-info = dicominfo (fullfile (filefolder, names {5}));
+info = dicominfo (fullfile (filefolder, names {5})); % get the relevant DICOM info from a random image slice
 numimages = length (5:length(names));
 
-Original = zeros (info.Rows, info.Columns, numimages, class(info.BitDepth));
+Original = zeros (info.Rows, info.Columns, numimages, class(info.BitDepth)); % creating an empty matrix with the relevant infomation
+
+
 %% rearranging the 3D matrix according to the instance number
 
-h = waitbar(0, 'Loading CT data');
+h = waitbar(0, 'Loading CT data'); % create waitbar
 order = [];
 
 % for all images in the folder
@@ -31,12 +33,12 @@ instance_no = info.InstanceNumber;
 % insert the image in the proper location of the matrix
 Original (:,:,instance_no) = uint16 (dicomread (fname));
 
-order (instance_no) = i;
+order (instance_no) = i; % make sure that the DICOM images load in sequence
 
 waitbar (i/length(names),h);
 end
 
-close (h)
+close (h) % close waitbar
 
 %% converting from gray values to HU
 Original = int16 (Original); % change from unsigned to signed integer to accomodate negative values
